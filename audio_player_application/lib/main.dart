1e78a68438374b0a37d0a1b652c3f4a319103e7c
Noah Beckman <36571775+Phoenix820@users.noreply.github.com>
2023-07-11 14:19:22 -0400

    Audio File application complete

---
+++ audio_player_application/lib/main.dart
@@ -0,0 +1,306 @@
+import 'package:audio_video_progress_bar/audio_video_progress_bar.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+import 'package:just_audio/just_audio.dart';
+import 'package:just_audio_background/just_audio_background.dart';
+import 'package:rxdart/rxdart.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Just Audio App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const AudioPlayerScreen(),
+    );
+  }
+}
+
+class PositionData {
+  const PositionData(this.position, this.bufferedPosition, this.duration);
+
+  final Duration position;
+  final Duration bufferedPosition;
+  final Duration duration;
+}
+
+class AudioPlayerScreen extends StatefulWidget {
+  const AudioPlayerScreen({super.key});
+
+  @override
+  State<AudioPlayerScreen> createState() => _AudioPlayerScreenState();
+}
+
+class _AudioPlayerScreenState extends State<AudioPlayerScreen> {
+  late AudioPlayer _audioPlayer;
+
+  final _playlist = ConcatenatingAudioSource(
+    children: [
+      AudioSource.uri(
+        Uri.parse('assets/audio/soft-music_davejf.mp3'),
+        tag: MediaItem(
+          id: '0',
+          title: 'Cool Music by Dave',
+          artist: 'Dave',
+          artUri: Uri.parse(
+              'https://freesound.org/data/avatars/11861/11861866_L.jpg'),
+        ),
+      ),
+      AudioSource.uri(
+        Uri.parse(
+          'https://traffic.megaphone.fm/LI9282413157.mp3?updated=1666999832',
+        ),
+        tag: MediaItem(
+          id: '1',
+          title: 'Some Podcast',
+          artist: 'People',
+          artUri: Uri.parse(
+              'https://e.snmc.io/i/300/s/7dfd48cb9c799dcf51e0d68f714f0168/3814281'),
+        ),
+      ),
+      AudioSource.uri(
+        Uri.parse(
+            'https://www.podtrac.com/pts/redirect.mp3/chrt.fm/track/143FF9/traffic.megaphone.fm/BIGPOC3958439873.mp3?updated=1688443816'),
+        tag: MediaItem(
+          id: '2',
+          title: 'BiggerPockets',
+          artist: 'Brandon Turner',
+          artUri: Uri.parse(
+              'https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcRErSIojJNSKv0fYOQfqGZ3DL3ChrzxmFtruT5RTMDvFe_7dmM'),
+        ),
+      ),
+    ],
+  );
+
+  Stream<PositionData> get _positionDataStream =>
+      Rx.combineLatest3<Duration, Duration, Duration?, PositionData>(
+        _audioPlayer.positionStream,
+        _audioPlayer.bufferedPositionStream,
+        _audioPlayer.durationStream,
+        (position, bufferedPosition, duration) =>
+            PositionData(position, bufferedPosition, duration ?? Duration.zero),
+      );
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _audioPlayer = AudioPlayer();
+    _init();
+  }
+
+  Future<void> _init() async {
+    await _audioPlayer.setLoopMode(LoopMode.all);
+    await _audioPlayer.setAudioSource(_playlist);
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    _audioPlayer.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        leading: IconButton(
+          onPressed: () {},
+          icon: const Icon(Icons.keyboard_arrow_down_rounded),
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.more_horiz),
+          ),
+        ],
+      ),
+      body: Container(
+        padding: const EdgeInsets.all(20),
+        height: double.infinity,
+        width: double.infinity,
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [Color(0xFF144771), Color(0xFF071A2C)],
+          ),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            StreamBuilder<SequenceState?>(
+              stream: _audioPlayer.sequenceStateStream,
+              builder: (context, snapshot) {
+                final state = snapshot.data;
+                if (state?.sequence.isEmpty ?? true) {
+                  return const SizedBox();
+                }
+                final metadata = state!.currentSource!.tag as MediaItem;
+                return MediaMetadata(
+                  imageUrl: metadata.artUri.toString(),
+                  title: metadata.title,
+                  artist: metadata.artist ?? '',
+                );
+              },
+            ),
+            StreamBuilder<PositionData>(
+              stream: _positionDataStream,
+              builder: (context, snapshot) {
+                final PositionData = snapshot.data;
+                return ProgressBar(
+                  barHeight: 8,
+                  baseBarColor: Colors.grey[600],
+                  bufferedBarColor: Colors.grey,
+                  progressBarColor: Colors.red,
+                  thumbColor: Colors.red,
+                  timeLabelTextStyle: const TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.w600,
+                  ),
+                  progress: PositionData?.position ?? Duration.zero,
+                  buffered: PositionData?.bufferedPosition ?? Duration.zero,
+                  total: PositionData?.duration ?? Duration.zero,
+                  onSeek: _audioPlayer.seek,
+                );
+              },
+            ),
+            const SizedBox(height: 20),
+            Controls(audioPlayer: _audioPlayer),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class MediaMetadata extends StatelessWidget {
+  const MediaMetadata({
+    super.key,
+    required this.imageUrl,
+    required this.title,
+    required this.artist,
+  });
+
+  final String imageUrl;
+  final String title;
+  final String artist;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        DecoratedBox(
+          decoration: BoxDecoration(
+            boxShadow: const [
+              BoxShadow(
+                color: Colors.black12,
+                offset: Offset(2, 4),
+                blurRadius: 4,
+              ),
+            ],
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: ClipRRect(
+              borderRadius: BorderRadius.circular(10),
+              child: CachedNetworkImage(
+                imageUrl: imageUrl,
+                height: 300,
+                width: 300,
+                fit: BoxFit.cover,
+              )),
+        ),
+        const SizedBox(height: 20),
+        Text(
+          title,
+          style: const TextStyle(
+            color: Colors.white,
+            fontSize: 22,
+            fontWeight: FontWeight.bold,
+          ),
+          textAlign: TextAlign.center,
+        ),
+        const SizedBox(height: 8),
+        Text(
+          artist,
+          style: const TextStyle(
+            color: Colors.white,
+            fontSize: 20,
+          ),
+          textAlign: TextAlign.center,
+        )
+      ],
+    );
+  }
+}
+
+class Controls extends StatelessWidget {
+  const Controls({
+    super.key,
+    required this.audioPlayer,
+  });
+
+  final AudioPlayer audioPlayer;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        IconButton(
+          onPressed: audioPlayer.seekToPrevious,
+          iconSize: 60,
+          color: Colors.white,
+          icon: const Icon(Icons.skip_previous_rounded),
+        ),
+        StreamBuilder<PlayerState>(
+          stream: audioPlayer.playerStateStream,
+          builder: (context, snapshot) {
+            final playerState = snapshot.data;
+            final processingState = playerState?.processingState;
+            final playing = playerState?.playing;
+            if (!(playing ?? false)) {
+              return IconButton(
+                onPressed: audioPlayer.play,
+                iconSize: 80,
+                color: Colors.white,
+                icon: const Icon(Icons.play_arrow_rounded),
+              );
+            } else if (processingState != ProcessingState.completed) {
+              return IconButton(
+                onPressed: audioPlayer.pause,
+                iconSize: 80,
+                color: Colors.white,
+                icon: const Icon(
+                  Icons.pause_rounded,
+                ),
+              );
+            }
+            return const Icon(
+              Icons.play_arrow_rounded,
+              size: 80,
+              color: Colors.white,
+            );
+          },
+        ),
+        IconButton(
+          onPressed: audioPlayer.seekToNext,
+          iconSize: 60,
+          color: Colors.white,
+          icon: const Icon(Icons.skip_next_rounded),
+        ),
+      ],
+    );
+  }
+}
